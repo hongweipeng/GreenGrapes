b16484af5fb94e5b218953dacf7477784648b694
HoneWeipeng <hongweichen8888@sina.com>
2016-10-17 08:16:07 +0800

    最新文章列表

--- sidebar.php
+++ sidebar.php
@@ -5,4 +5,24 @@
             <button class="btn btn-success btn-small"><i class="fa fa-search"></i> 查找</button>
         </form>
     </aside>
+    <aside id="zan_posts_category-2">
+        <div class="panel panel-success hidden-xs">
+            <div class="panel-heading">文章分类</div>
+            <ul class="list-group">
+                <li class="list-group-item clearfix">
+                    <a href="http://www.linuxhot.com/cloud-computing.html">
+                        云计算三大服务模式：SAAS、PAAS、IAAS                </a>
+                </li>
+                <li class="list-group-item clearfix">
+                    <a href="http://www.linuxhot.com/docker-1-5.html">
+                        Docker 1.5 正式发布，完全支持 IPv6                </a>
+                </li>
+                <li class="list-group-item clearfix">
+                    <a href="http://www.linuxhot.com/elasticsearch-1-3-81-4-3.html">
+                        ElasticSearch 1.3.8/1.4.3 发布，分布式搜索引擎                </a>
+                </li>
+
+            </ul>
+        </div>
+    </aside>
 </aside>
