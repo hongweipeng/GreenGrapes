2e3a08d4e4c8549cf6c2a0c9e1cc51f6e14b5530
riddle <riddle911@foxmail.com>
2017-08-19 23:21:02 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 <footer id="m-footer">
     <div class="Copyright">
         <p>&copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
-        <?php _e('All Rights Reserved. 版权所有.'); ?>
+        <?php _e('All Rights Reserved. 版权所有.<br><a href="https://github.com/hongweipeng">Theme by HongWeipeng</a>'); ?>
             <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1257015934'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1257015934%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
         </p>
         <p>
