558125e7dff86cd8f1109bd6d334e249ebf683c2
HongWeipeng <hongweichen8888@sina.com>
2018-12-26 10:19:05 +0800

    fix

--- sidebar.php
+++ sidebar.php
@@ -29,16 +29,18 @@
         </div>
     </aside>
     <aside>
-        <!-- 侧边栏 -->
-        <ins class="adsbygoogle"
-             style="display:block"
-             data-ad-client="ca-pub-7489805328585400"
-             data-ad-slot="8357527080"
-             data-ad-format="auto"
-             data-full-width-responsive="true"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
+        <div style="margin-bottom: 20px;">
+            <!-- 侧边栏 -->
+            <ins class="adsbygoogle"
+                 style="display:block"
+                 data-ad-client="ca-pub-7489805328585400"
+                 data-ad-slot="8357527080"
+                 data-ad-format="auto"
+                 data-full-width-responsive="true"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
     </aside>
     <?php if(class_exists('Links_Plugin') && isset($this->options->plugins['activated']['Links'])): ?>
     <aside>
