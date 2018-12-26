5cd1303fbe3d29dd87210d14d6d3548d664e0f6f
HongWeipeng <hongweichen8888@sina.com>
2018-12-26 09:53:11 +0800

    侧边栏广告

--- sidebar.php
+++ sidebar.php
@@ -28,6 +28,18 @@
             </div>
         </div>
     </aside>
+    <aside>
+        <!-- 侧边栏 -->
+        <ins class="adsbygoogle"
+             style="display:block"
+             data-ad-client="ca-pub-7489805328585400"
+             data-ad-slot="8357527080"
+             data-ad-format="auto"
+             data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </aside>
     <?php if(class_exists('Links_Plugin') && isset($this->options->plugins['activated']['Links'])): ?>
     <aside>
         <div class="panel panel-green hidden-xs">
