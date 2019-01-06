dbb050ac89d5b522076bf430808f577972c419b7
HongWeipeng <hongweichen8888@sina.com>
2019-01-06 19:45:39 +0800

    修复分类页样式问题

--- archive.php
+++ archive.php
@@ -27,12 +27,15 @@
                             </div>
                             <hr>
                             <div class="pull-left">
-                                <a class="btn btn-green" href="<?php $this->permalink() ?>">阅读全文</a>
+                                <a class="btn btn-skin" href="<?php $this->permalink() ?>">阅读全文</a>
                             </div>
                             <div class="pull-right post-info">
                                 <span><i class="fa fa-calendar"></i> <?php $this->date('Y-m-d'); ?></span>
                                 <span><i class="fa fa-user"></i> <a href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></span>
                                 <span><i class="fa fa-comment"></i> <a href="<?php $this->permalink() ?>"><?php $this->commentsNum('%d 条评论'); ?></a></span>
+                                <?php if (class_exists('TeStat_Plugin') && isset($this->options->plugins['activated']['TeStat'])): ?>
+                                    <span><i class="fa fa-fw fa-eye"></i> <?php $this->viewsNum(); ?> 次浏览</span>
+                                <?php endif; ?>
                             </div>
                         </section>
                     </article>
