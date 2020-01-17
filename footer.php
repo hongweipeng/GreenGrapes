4d37a47c90bb4d13475b51e82d5cfb36e35c3939
hongweipeng <hongweichen8888@sina.com>
2020-01-17 17:16:17 +0800

    备案信息添加链接地址

--- footer.php
+++ footer.php
@@ -25,7 +25,7 @@
             <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1257015934'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1257015934%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
         </p>
         <p>
-            闽ICP备15027255号-1
+            <a href="http://www.beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">闽ICP备15027255号-1</a>
         </p>
     </div>
 </footer>
