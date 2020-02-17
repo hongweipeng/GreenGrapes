7b552b77592415ca299e2573dad6d9483765f6c0
hongweipeng <hongweichen8888@sina.com>
2020-02-17 10:34:22 +0800

    由于畅言引入jquery失败

--- footer.php
+++ footer.php
@@ -41,8 +41,8 @@
 <script src="<?php $this->options->themeUrl('js/home.js'); ?>"></script>
 
 <script id="cy_cmt_num" src="https://changyan.sohu.com/upload/plugins/plugins.list.count.js?clientId=cysUkopEY"></script>
-<script type="text/javascript" charset="utf-8" src="https://changyan.itc.cn/js/lib/jquery.js"></script>
-<script type="text/javascript" charset="utf-8" src="https://changyan.sohu.com/js/changyan.labs.https.js?appid=cysUkopEY"></script>
+<!--<script type="text/javascript" charset="utf-8" src="https://changyan.itc.cn/js/lib/jquery.js"></script>-->
+<!--<script type="text/javascript" charset="utf-8" src="https://changyan.sohu.com/js/changyan.labs.https.js?appid=cysUkopEY"></script>-->
 <?php if($this->is('post') or $this->is('single')): ?>
 <!-- 畅言 start-->
 <script charset="utf-8" type="text/javascript" src="https://changyan.sohu.com/upload/changyan.js" ></script>
