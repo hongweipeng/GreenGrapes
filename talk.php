0281eb01004134f03a1ba4c1d90b9b8d630e3aaf
hongweipeng <hongweichen8888@sina.com>
2020-05-22 15:33:00 +0800

    完善说说模板

--- talk.php
+++ talk.php
@@ -5,30 +5,29 @@
  * @package custom
  */
 $this->need('header.php');
-$options = Typecho_Widget::widget('Widget_Options');
 ?>
     <style>
-        .tmtimeline {
+        .tk-timeline {
             position: relative;
             margin: 30px 0 0;
             padding: 0;
             list-style: none
         }
 
-        .tmtimeline .line {
+        .tk-timeline .line {
             position: relative;
             min-height: 80px
         }
 
-        .tmtimeline .author-gravatar {
+        .tk-timeline .author-gravatar {
             border-radius: 50%;
         }
 
-        .tmtimeline .line:last-child {
+        .tk-timeline .line:last-child {
             min-height: 0
         }
 
-        .tmtimeline:before {
+        .tk-timeline:before {
             position: absolute;
             top: 0;
             bottom: 0;
@@ -39,36 +38,29 @@ $options = Typecho_Widget::widget('Widget_Options');
             content: ''
         }
 
-        .tmtimeline > .line .tmtime {
+        .tk-timeline > .line .tk-time {
             position: absolute;
             display: block;
             padding-right: 100px;
             width: 20%
         }
 
-        .tmtimeline > .line .tmtime span {
+        .tk-timeline > .line .tk-time {
             display: block;
-            text-align: right
+            text-align: right;
+            color: #6cbfee;
+            font-size: 1.1em;
         }
 
-        .tmtimeline > .line .tmtime span:first-child {
-            color: #bdd0db;
-            font-size: .9em
-        }
-
-        .tmtimeline > .line .tmtime span:last-child {
+        .tk-timeline > .line .tk-time-lg {
             color: #3594cb;
-            font-size: 2.9em
+            font-size: 2em
         }
 
-        .tmtimeline > .line:nth-child(odd) .tmtime span:last-child {
-            color: #6cbfee
-        }
-
-        .tmtimeline > .line .tmlabel {
+        .tk-timeline > .line .tk-label {
             position: relative;
             margin: 0 0 15px 20%;
-            padding: 1em 2em 2em;
+            padding: 1em 1em 2em;
             border-radius: 4px;
             background: #3594cb;
             color: #fff;
@@ -77,23 +69,15 @@ $options = Typecho_Widget::widget('Widget_Options');
             line-height: 1.4
         }
 
-        .tmtimeline .box {
+        .tk-timeline .box {
             margin: 5px 0 15px 20%
         }
 
-        .tmtimeline > .line:nth-child(odd) .tmlabel {
+        /*.tk-timeline > .line:nth-child(odd) .tk-label {
             background: #6cbfee
-        }
+        }*/
 
-        .tmtimeline > .line .tmlabel h2 {
-            margin-top: 0;
-            padding: 0 0 10px;
-            border-bottom: 1px solid rgba(255, 255, 255, .4);
-            color: #fff;
-            font-size: 22px
-        }
-
-        .tmtimeline > .line .tmlabel:after {
+        .tk-timeline > .line .tk-label:after {
             position: absolute;
             top: 10px;
             right: 100%;
@@ -106,11 +90,11 @@ $options = Typecho_Widget::widget('Widget_Options');
             border-right-color: #3594cb
         }
 
-        .tmtimeline > .line:nth-child(odd) .tmlabel:after {
+        /*.tk-timeline > .line:nth-child(odd) .tk-label:after {
             border-right-color: #6cbfee
-        }
+        }*/
 
-        .tmtimeline > .line .tmicon {
+        .tk-timeline > .line .tk-icon {
             position: absolute;
             top: 0;
             left: 15%;
@@ -127,50 +111,54 @@ $options = Typecho_Widget::widget('Widget_Options');
             font-style: normal;
             font-variant: normal;
             font-size: 1.4em;
-            line-height: 40px;
             speak: none;
             -webkit-font-smoothing: antialiased
         }
-        .tmtimeline .page-nav {
+        .tk-timeline > .line .fa-icon {
+            line-height: 40px;
+        }
+        .tk-timeline .page-nav {
             margin: 5px 0 15px 20%;
         }
     </style>
     <div id="m-container" class="container">
         <div class="row ml-0 mr-0">
-            <div class="col-md-8 pl-0 pr-0">
+            <div class="col-md-8 pl-1 pr-1">
                 <?php
-                    $page = 1;
-                    $page_size = 2;
-                    $total = MicroTalk_Plugin::totalCount();
+                    $page = (int) $this->request->get('page', 1);
+                    $page_size = 20;
+                    $total = MicroTalk_Plugin::totalShowCount();
                     $talks = MicroTalk_Plugin::talkPosts($page, $page_size);
                 ?>
-                <ul class="tmtimeline" id="timeline">
+                <ul class="tk-timeline">
+                    <?php $year = ''; ?>
                     <?php foreach ($talks as $talk) : ?>
                     <li class="line">
-                        <time class="tmtime" datetime=""><span><?php _e($talk['created']); ?></span> <span title="">'H:i'</span></time>
-                        <div class="tmicon fa fa-comment-o"></div>
-                        <div class="tmlabel">
-                            <h2>
-                                <img class="author-gravatar" src="<?php _e(Typecho_Common::gravatarUrl($talk['mail'], 40, 'X', 'mm', $this->request->isSecure())); ?>" alt="<?php _e($talk['author']); ?>" />
-                                <?php _e($talk['author']); ?>
-                            </h2>
+                        <time class="tk-time" datetime="">
+                            <?php if ($year !== date('Y', $talk['created'])) :?>
+                            <span class="tk-time-lg"><?php _e(date('Y', $talk['created'])); ?></span>
+                            <?php endif; ?>
+                            <span><?php _e(date('m-d', $talk['created'])); ?></span>
+                        </time>
+                        <div class="tk-icon">
+                            <img class="author-gravatar" src="<?php _e(Typecho_Common::gravatarUrl($talk['mail'], 40, 'X', 'mm', $this->request->isSecure())); ?>" alt="<?php _e($talk['author']); ?>" />
+                        </div>
+                        <div class="tk-label">
                             <div class="index-text post-content">
-                                <p><?php _e($talk['content']); ?></p>
+                                <?php _e($talk['content']); ?>
                             </div>
                         </div>
                     </li>
+                    <?php $year = date('Y', $talk['created']); ?>
                     <?php endforeach; ?>
                     <li class="line">
-                        <div class="tmicon fa fa-ellipsis-h"></div>
+                        <div class="tk-icon fa-icon fa fa-ellipsis-h"></div>
                         <div class="page-nav">
                             <nav>
                             <?php
                                 //分页
-                                $currUrl = ($this->request->isSecure() ? 'https://' : 'http://').$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
-                                parse_str($_SERVER['QUERY_STRING'], $parseUrl);
-                                unset($parseUrl['page']);
-                                $query = $currUrl.'?'.http_build_query($parseUrl);
-                                $pange_nav = new Typecho_Widget_Helper_PageNavigator_Box($total, $page, $page_size,$query.'&page={page}');
+                                $query = $this->request->makeUriByRequest('page={page}');
+                                $pange_nav = new Typecho_Widget_Helper_PageNavigator_Box($total, $page, $page_size,$query);
                                 echo '<ul class="pagination">';
                                 $pange_nav->render('&laquo;', '&raquo;', 3, '...', array(
                                     'itemTag'       =>  'li',
