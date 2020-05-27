0f0384a8fdedd290359dc5c02682c650804d53bc
hongweipeng <hongweichen8888@sina.com>
2020-05-27 15:55:28 +0800

    移除gif load

--- footer.php
+++ footer.php
@@ -40,7 +40,6 @@
 <script src="<?php $this->options->themeUrl('js/particles.min.js'); ?>"></script>
 <script src="<?php $this->options->themeUrl('js/headerCanvas.js'); ?>"></script>
 <script src="<?php $this->options->themeUrl('js/home.js'); ?>"></script>
-<script src="<?php $this->options->themeUrl('js/load-gif.js'); ?>"></script>
 
 <script>
     // 外链新窗口打开以及添加noopener
@@ -53,14 +52,6 @@
                         rel: 'noopener'
                     });
                 }
-            });
-            $('.container img').each(function(_, obj) {
-                let $obj = $(obj);
-                let src = $obj.attr('src');
-                if (!src || src.indexOf('.gif') < 0) {
-                    return;
-                }
-                $obj.gif();
             });
         });
     })(jQuery);
