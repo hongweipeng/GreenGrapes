c21cab20350c06dcde7ce941409cf0812e8e40b5
hongweipeng <hongweichen8888@sina.com>
2022-01-20 17:26:09 +0800

    代码结构调整

--- page.php
+++ page.php
@@ -7,8 +7,8 @@ $this->need('header.php');
         <div class="col-md-8 pl-0 pr-0">
             <div id="article-list">
                 <article class="post-article clearfix">
-                    <div>
-                        <h2 class="title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
+                    <div class="title">
+                        <h2><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
                         <p class="post-big-info">
                             <span class="badge badge-skin"><i class="fa fa-fw fa-user"></i> <a href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></span>
                             <span class="badge badge-skin"><i class="fa fa-fw fa-calendar"></i> <?php $this->date('Y-m-d'); ?></span>
