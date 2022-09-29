a2cb9caf9f3b1863dd37dabcdd7f7bf52323191d
hongweipeng <hongweichen8888@sina.com>
2022-09-29 22:58:31 +0800

    更新bootstrap到4.6.1

--- footer.php
+++ footer.php
@@ -27,9 +27,7 @@
     </div>
 </footer>
 <script src="<?php $this->options->themeUrl('js/jquery.min.js'); ?>"></script>
-<script src="<?php $this->options->themeUrl('js/popper.min.js'); ?>"></script>
-<script src="//cdn.staticfile.org/popper.js/1.15.0/umd/popper.min.js"></script>
-<script src="<?php $this->options->themeUrl('js/bootstrap.min.js'); ?>"></script>
+<script src="<?php $this->options->themeUrl('js/bootstrap.bundle.min.js'); ?>"></script>
 <script src="<?php $this->options->themeUrl('js/jquery.cookie.js'); ?>"></script>
 <script src="<?php $this->options->themeUrl('js/functionall.js'); ?>"></script>
 <script src="<?php $this->options->themeUrl('js/tagcanvas.min.js'); ?>"></script>
